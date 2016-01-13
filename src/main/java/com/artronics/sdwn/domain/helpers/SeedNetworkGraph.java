26e81e783f2c027e46c1c034fa2388c84d5b0edc
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-13 16:23:25 +0100

    fix the problem with copy seeder by inheritance

--- src/main/java/com/artronics/sdwn/domain/helpers/SeedNetworkGraph.java
+++ src/main/java/com/artronics/sdwn/domain/helpers/SeedNetworkGraph.java
@@ -12,55 +12,91 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
+
+ /*
+     Device1
+     node number 30 is sameAddNode
+     look for code for weight values
+     Graph is like
+                 sink:0
+                   /   \
+                 w50  w10
+                 /      \
+              135 --w20-- 30
+                \         /
+                w25    w100
+                  \    /
+                   136
+                    |
+                   w30
+                    |
+                   137
+
+     Device2
+     node number 30 is sameAddNode
+     Graph is like
+                 sink
+                     |
+                    w10
+                     |
+                    245
+                   /   \
+                 w20    w100
+                /         \
+              246 --w30-- 30
+  */
+
+
 public class SeedNetworkGraph
 {
-    private final static Logger log = Logger.getLogger(SeedNetworkGraph.class);
-    private NetworkSession activeSession = new NetworkSession();
-    private NetworkSession expiredSession = new NetworkSession();
+    protected final static Logger log = Logger.getLogger(SeedNetworkGraph.class);
+    protected NetworkSession activeSession = new NetworkSession();
+    protected NetworkSession expiredSession = new NetworkSession();
 
-    private SdwnControllerEntity controller = new SdwnControllerEntity("http://controller:port");
+    protected SdwnControllerEntity controller = new SdwnControllerEntity("http://controller:port");
 
-    private DeviceConnectionEntity device1 = new DeviceConnectionEntity("http://device1:port");
-    private DeviceConnectionEntity device2 = new DeviceConnectionEntity("http://device2:port");
+    protected DeviceConnectionEntity device1 = new DeviceConnectionEntity("http://device1:port");
+    protected DeviceConnectionEntity device2 = new DeviceConnectionEntity("http://device2:port");
 
-    private SdwnNodeEntity sink1 = new SdwnNodeEntity(0L, device1);
-    private SdwnNodeEntity sink2 = new SdwnNodeEntity(1L, device2);
+    protected SdwnNodeEntity sink1 = new SdwnNodeEntity(0L, device1);
+    protected SdwnNodeEntity sink2 = new SdwnNodeEntity(1L, device2);
 
-    private SdwnNodeEntity sameAddNode1 = new SdwnNodeEntity(30L, device1);
-    private SdwnNodeEntity sameAddNode2 = new SdwnNodeEntity(30L, device2);
+    protected SdwnNodeEntity sameAddNode1 = new SdwnNodeEntity(30L, device1);
+    protected SdwnNodeEntity sameAddNode2 = new SdwnNodeEntity(30L, device2);
 
-    private SdwnNodeEntity node135 = new SdwnNodeEntity(135L, device1);
-    private SdwnNodeEntity node136 = new SdwnNodeEntity(136L, device1);
-    private SdwnNodeEntity node137 = new SdwnNodeEntity(137L, device1);
+    protected SdwnNodeEntity node135 = new SdwnNodeEntity(135L, device1);
+    protected SdwnNodeEntity node136 = new SdwnNodeEntity(136L, device1);
+    protected SdwnNodeEntity node137 = new SdwnNodeEntity(137L, device1);
 
-    private SdwnNodeEntity node245 = new SdwnNodeEntity(245L, device2);
-    private SdwnNodeEntity node246 = new SdwnNodeEntity(246L, device2);
+    protected SdwnNodeEntity node245 = new SdwnNodeEntity(245L, device2);
+    protected SdwnNodeEntity node246 = new SdwnNodeEntity(246L, device2);
 
-    private SessionRepo sessionRepo;
-    private SdwnControllerRepo controllerRepo;
-    private DeviceConnectionRepo deviceRepo;
-    private NodeRepo nodeRepo;
-    private NeighborRepo neighborRepo;
+    protected SessionRepo sessionRepo;
+    protected SdwnControllerRepo controllerRepo;
+    protected DeviceConnectionRepo deviceRepo;
+    protected NodeRepo nodeRepo;
+    protected NeighborRepo neighborRepo;
 
-    private boolean persist = false;
+    protected boolean persist = false;
 
     public void seed(boolean persist)
     {
         createNetwork();
-        persistNetwork(persist);
+        if (persist)
+            persistNetwork();
+        else
+            mockPersistNetwork();
 
-        if (persist) {
-            createNodes(expiredSession);
-//            persistNodes(persist);
-            sessionRepo.save(expiredSession);
-        }
         createNodes(activeSession);
+        if (persist)
+            persistNodes();
+        else
+            mockPersistNodes();
 
-        persistNodes(persist);
         persistNodeLinks();
     }
 
-    private void createNetwork()
+    protected void createNetwork()
     {
         expiredSession.setStatus(NetworkSession.Status.EXPIRED);
 
@@ -71,7 +107,7 @@ public class SeedNetworkGraph
         device2.setSinkAddress(1L);
     }
 
-    private void createNodes(NetworkSession session)
+    protected void createNodes(NetworkSession session)
     {
         sink1.setSession(session);
         sink2.setSession(session);
@@ -89,108 +125,73 @@ public class SeedNetworkGraph
         node246.setSession(session);
     }
 
-    private void persistNetwork(boolean persist)
+    protected void mockPersistNetwork()
     {
-        if (persist) {
-            controllerRepo.save(controller);
+        expiredSession.setId(1L);
+        activeSession.setId(2L);
 
-            sessionRepo.save(expiredSession);
-            sessionRepo.save(activeSession);
+        controller.setId(1L);
 
-            deviceRepo.save(device1);
-            deviceRepo.save(device2);
-        }else {
-            expiredSession.setId(1L);
-            activeSession.setId(2L);
-
-            controller.setId(1L);
-
-            device1.setId(1L);
-            device2.setId(2L);
-        }
+        device1.setId(1L);
+        device2.setId(2L);
     }
 
-    private void persistNodes(boolean persist)
+    protected void persistNetwork()
     {
-        if (persist) {
-            nodeRepo.save(sink1);
-            nodeRepo.save(sink2);
+        controllerRepo.save(controller);
 
-            nodeRepo.save(sameAddNode1);
-            nodeRepo.save(sameAddNode2);
+        sessionRepo.save(expiredSession);
+        sessionRepo.save(activeSession);
 
-            nodeRepo.save(node135);
-            nodeRepo.save(node136);
-            nodeRepo.save(node137);
+        deviceRepo.save(device1);
+        deviceRepo.save(device2);
+    }
 
-            nodeRepo.save(node245);
-            nodeRepo.save(node246);
-        }else {
-            sink1.setId(0L);
-            sink2.setId(1L);
+    protected void mockPersistNodes()
+    {
+        sink1.setId(0L);
+        sink2.setId(1L);
 
-            sameAddNode1.setId(100L);
-            sameAddNode2.setId(200L);
+        sameAddNode1.setId(100L);
+        sameAddNode2.setId(200L);
 
-            node135.setId(135L);
-            node136.setId(136L);
-            node137.setId(137L);
+        node135.setId(135L);
+        node136.setId(136L);
+        node137.setId(137L);
 
-            node245.setId(245L);
-            node246.setId(246L);
-        }
+        node245.setId(245L);
+        node246.setId(246L);
+    }
+
+    protected void persistNodes()
+    {
+        nodeRepo.save(sink1);
+        nodeRepo.save(sink2);
+
+        nodeRepo.save(sameAddNode1);
+        nodeRepo.save(sameAddNode2);
+
+        nodeRepo.save(node135);
+        nodeRepo.save(node136);
+        nodeRepo.save(node137);
+
+        nodeRepo.save(node245);
+        nodeRepo.save(node246);
     }
 
     @Transactional
-    private void persistNodeLinks(){
-        /*
-            Device1
-            node number 30 is sameAddNode
-            look for code for weight values
-            Graph is like
-                        sink:0
-                          /   \
-                        w50  w10
-                        /      \
-                     135 --w20-- 30
-                       \         /
-                       w25    w100
-                         \    /
-                          136
-                           |     |
-                          w30
-                          /
-                        137
-
-         */
+    protected void persistNodeLinks()
+    {
 
 
-
-        /*
-            Device2
-            node number 30 is sameAddNode
-            look for code for weight values
-            Graph is like
-                        sink
-                            |
-                           w10
-                            |
-                           245
-                          /   \
-                        w20    w100
-                       /         \
-                     246 --w30-- 30
-
-         */
-
-        SdwnNeighbor n1_245 = new SdwnNeighbor(sink2,node245,10D,245);
+        SdwnNeighbor n1_245 = new SdwnNeighbor(sink2, node245, 10D, 245);
 //        sink2.getNeighbors().add(n1_245);
 //        neighborRepo.save(n1_245);
 //        nodeRepo.save(sink2);
 
-        SdwnNeighbor n245_246 = new SdwnNeighbor(node245,node246,20D,235);
+        SdwnNeighbor n245_246 = new SdwnNeighbor(node245, node246, 20D, 235);
 //        neighborRepo.persist(n245_246,node245);
-        SdwnNeighbor n245_30 = new SdwnNeighbor(node245,sameAddNode2,100D,155);
+        SdwnNeighbor n245_30 = new SdwnNeighbor(node245, sameAddNode2, 100D, 155);
 //        neighborRepo.persist(n245_30,node245);
 //        node245.getNeighbors().add(n245_30);
 //        node245.getNeighbors().add(n245_246);
