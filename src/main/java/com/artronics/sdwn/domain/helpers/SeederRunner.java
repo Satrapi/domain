3ce163cc98dd9a0f97d017d55bc68c09e01837b1
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-16 07:26:27 +0100

    remove SeederRunner. I dont know why but during controller startup seeder runs

--- src/main/java/com/artronics/sdwn/domain/helpers/SeederRunner.java
+++ src/main/java/com/artronics/sdwn/domain/helpers/SeederRunner.java
@@ -1,7 +1,6 @@
 package com.artronics.sdwn.domain.helpers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -9,12 +8,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @ComponentScan(basePackages = "com.artronics.sdwn.domain.helpers")
-public class SeederRunner implements ApplicationListener<ContextRefreshedEvent>
+public class SeederRunner //implements ApplicationListener<ContextRefreshedEvent>
 {
     @Autowired
     private SeedNetworkGraph seeder;
 
-    @Override
+//    @Override
     @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event)
     {
